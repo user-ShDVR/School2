0c7a09ff6d062c94df8dd2c30a97912e34f9e50d
Miroslav <bangwars3@gmail.com>
2023-07-05 20:09:45 +0500

    add jest, add first test

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -18,6 +18,27 @@ export function buildLoaders({ isDev, paths }: BuildOptions): webpack.RuleSetRul
         use: 'ts-loader',
         exclude: /node_modules/,
     };
+
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        'i18next-extract',
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true,
+                        },
+                    ],
+                ],
+            },
+        },
+    };
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -38,6 +59,7 @@ export function buildLoaders({ isDev, paths }: BuildOptions): webpack.RuleSetRul
     return [
         imgLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ];
